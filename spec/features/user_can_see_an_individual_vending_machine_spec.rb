36947fffcaaaa6b42c886a86c67f20451f4747a9
Emily Wise <wise.m.emily@gmail.com>
2017-11-03 10:18:37 -0600

    Add test for first user story

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,21 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario "they see the name and price of all the snacks for that machine" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: "White Castle Burger", price: 3.50)
+    snack_2 = dons.snacks.create(name: "Pop Rocks", price: 1.50)
+    snack_3 = dons.snacks.create(name: "Flaming Hot Cheetos", price: 2.50)
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("White Castle Burger: $3.50")
+    expect(page).to have_content("Pop Rocks: $1.50")
+    expect(page).to have_content("Flaming Hot Cheetos: $2.50")
+  end
+
+
+
 end
